a6f308c0ec49d7a22d4340cebfc856ea6c254441
spectocr <highstakes22@gmail.com>
2021-05-08 10:02:10 -0400

    fixed the bug of not using criteria

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 //spell out the arrays
-  var lcarr = "a b c d e f g h i j k l m n o p q u r t u v w x y z";
+  var lcarr = "abcdefghijklmnopqurtuvwxyz";
   var lcarr2 = lcarr.split(' ');
-  var ucarr = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+  var ucarr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var ucarr2 = ucarr.split(' ');
   console.log(ucarr2);
-  var nums = "1 2 3 4 5 6 7 8 9 0";
+  var nums = "1234567890";
   var nums2 = nums.split(' ');
-  var spcchar1 = "! @ # $ % ^ & * ( )";
+  var spcchar1 = "!@#$%^&*()";
   var spcchar2 = spcchar1.split(' ');
 
 
 function generatePassword() {
   var passwordText = document.querySelector("#password");
   var pwlength = window.prompt("Choose Password length, at least 8 characters and no more than 128 characters")
-  if (pwlength < 8 || pwlength > 128) {
+  if(pwlength < 8 || pwlength > 128) {
     alert("too little or too much, try agin")
+    generatePassword();
   };
+  //more testing
+//test 
+var arraytouse = ""
+
  var lowercase = confirm("you want lowercase letters?");
+ if (lowercase == true) {
+  arraytouse = lcarr;
+  console.log("array to use is " + arraytouse);
+ }
  var uppercase = confirm("What about UPPERCASE?");
+ if (uppercase == true) {
+   arraytouse = arraytouse.concat(ucarr);
+   console.log("array to use is " + arraytouse);
+ }
  var numeric = confirm("you want numbers?");
+ if (numeric == true){
+   arraytouse = arraytouse.concat(nums);
+   console.log("array to use is " + arraytouse);
+ }
  var spcchar = confirm("what about special characters? (?!@#$%^&*)");
+ if (spcchar == true) {
+  arraytouse = arraytouse.concat(spcchar1);
+  console.log("array to use is " + arraytouse);
+ }
  if (!lowercase && !uppercase && !numeric && !spcchar) {
    alert("you must pick some criteria");
    generatePassword();
  };
+
+
+ /*/test ground
+var arraytouse = "";
+
+if (lowercase == true && uppercase == true && numeric == true && spcchar ==true) {
+  arraytouse = lcarr2.concat(ucarr2).concat(nums2).concat(spcchar2);
+  console.log("array to use is " + arraytouse);
+}
+
+
+ //end test ground */
+
+
   var length = pwlength,
-      charset1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()",
-      charset2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      charset3 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+     // charset1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()",
+     // charset2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+     // charset3 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 
       retVal = "";
       console.log(retVal);
-  for (var i = 0, n = charset1.length; i < length; ++i) {
-      retVal += charset1.charAt((Math.random() * n));
+  for (var i = 0, n = arraytouse.length; i < length; ++i) {
+      retVal += arraytouse.charAt((Math.random() * n));
   }
   console.log(retVal);
   passwordText.value = retVal;
